36b62896715082fa7cb3450d09dd867354f9442a
yuxuanchiadm <yuxuanchiadm@126.com>
2020-03-17 05:43:03 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Monadium [![Build Status](https://travis-ci.com/yuxuanchiadm/monadium.svg?branch=master)](https://travis-ci.com/yuxuanchiadm/monadium)
+
+Monadic functional library for Java.
+
+# Build
+
+**IMPORTANT**: Please use `OpenJDK 11` to build this project.
+
+Although all sourcecode is written in `Java 8` and compiled to `Java 8` bytecode.
+But `OpenJDK 8` has some bugs in type inference.
+So some polymorphic type variables won't inferred correctly.
+Therefore some code won't compile using `OpenJDK 8`.
---
+++ monadium-core/src/test/java/org/monadium/core/control/FunctionalTest.java
@@ -0,0 +1,24 @@
+package org.monadium.core.control;
+
+import org.monadium.core.control.Functional;
+import static org.monadium.core.control.Functional.*;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.stream.IntStream;
+
+public class FunctionalTest {
+	@Test public void testBasic() {
+		assertThrows(NullPointerException.class, () -> fix(self -> null).apply(0));
+		assertThrows(StackOverflowError.class, () -> fix(self -> self).apply(0));
+		assertThrows(StackOverflowError.class, () -> fix(self -> { self.apply(0); return self; }).apply(0));
+		assertTimeout(Duration.ofSeconds(1), () -> fix(self -> null));
+		assertTimeout(Duration.ofSeconds(1), () -> fix(self -> self));
+		assertTimeout(Duration.ofSeconds(1), () -> fix(self -> { self.apply(0); return self; }));
+		assertEquals(IntStream.rangeClosed(0, 16).sum(), Functional.<Integer, Integer> fix(self -> x ->
+			x >= 0 ? x + self.apply(x - 1) : 0
+																						  ).apply(16));
+	}
+}
