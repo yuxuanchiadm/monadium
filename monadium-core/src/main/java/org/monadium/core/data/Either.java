ed0ff6796853529efd252b054dbfbfb4d55a87cf
yuxuanchiadm <yuxuanchiadm@126.com>
2020-08-05 22:54:47 +0800

    Add either monad

--- monadium-core/src/main/java/org/monadium/core/data/Either.java
+++ monadium-core/src/main/java/org/monadium/core/data/Either.java
@@ -2,8 +2,12 @@ package org.monadium.core.data;
 
 import org.monadium.core.data.Bottom;
 import static org.monadium.core.data.Bottom.*;
+import org.monadium.core.data.Unit;
+import static org.monadium.core.data.Unit.*;
 
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public abstract class Either<A, B> {
 	public static final class Left<A, B> extends Either<A, B> {
@@ -20,6 +24,10 @@ public abstract class Either<A, B> {
 		@Override public B fromRight(B other) { return other; }
 		@Override public A coerceLeft() throws Undefined { return a; }
 		@Override public B coerceRight() throws Undefined { return undefined(); }
+
+		@Override public <C> Either<A, C> map(Function<B, C> f) { return left(a); }
+		@Override public <C> Either<A, C> applyMap(Either<A, Function<B, C>> fbc) { return fbc.flatMap(f -> map(f)); }
+		@Override public <C> Either<A, C> flatMap(Function<B, Either<A, C>> f) { return left(a); }
 
 		@Override public boolean equals(Object x) { return x instanceof Left && Objects.equals(a, ((Left<?, ?>) x).a); }
 		@Override public int hashCode() { return Objects.hash(1, a); }
@@ -38,6 +46,10 @@ public abstract class Either<A, B> {
 		@Override public B fromRight(B other) { return b; }
 		@Override public A coerceLeft() throws Undefined { return undefined(); }
 		@Override public B coerceRight() throws Undefined { return b; }
+
+		@Override public <C> Either<A, C> map(Function<B, C> f) { return right(f.apply(b)); }
+		@Override public <C> Either<A, C> applyMap(Either<A, Function<B, C>> fbc) { return fbc.flatMap(f -> map(f)); }
+		@Override public <C> Either<A, C> flatMap(Function<B, Either<A, C>> f) { return f.apply(b); }
 
 		@Override public boolean equals(Object x) { return x instanceof Right && Objects.equals(b, ((Right<?, ?>) x).b); }
 		@Override public int hashCode() { return Objects.hash(2, b); }
@@ -59,6 +71,21 @@ public abstract class Either<A, B> {
 	public abstract A coerceLeft() throws Undefined;
 	public abstract B coerceRight() throws Undefined;
 
+	public abstract <C> Either<A, C> map(Function<B, C> f);
+	public abstract <C> Either<A, C> applyMap(Either<A, Function<B, C>> fbc);
+	public abstract <C> Either<A, C> flatMap(Function<B, Either<A, C>> f);
+
+	public static <A, B> Either<A, B> pure(B b) { return right(b); }
+	public static <A, B, C> Either<A, C> replace(Either<A, B> fb, C c) { return fb.map(b -> c); }
+	public static <A, B> Either<A, Unit> discard(Either<A, B> fb) { return fb.map(b -> unit()); }
+
 	@Override public abstract boolean equals(Object x);
 	@Override public abstract int hashCode();
+
+	public static final class Notation {
+		Notation() {}
+
+		public static <A, B, C> Either<A, C> $(Either<A, B> fb, Function<B, Either<A, C>> f) { return fb.flatMap(f); }
+		public static <A, B, C> Either<A, C> $(Either<A, B> fb, Supplier<Either<A, C>> fc) { return fb.flatMap(a -> fc.get()); }
+	}
 }
