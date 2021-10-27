f908aa72f7f11217380e8f51b8500560e66af133
yuxuanchiadm <yuxuanchiadm@126.com>
2021-10-27 13:21:44 +0800

    Fix access modifiers for Bottom

--- monadium-core/src/main/java/org/monadium/core/data/Bottom.java
+++ monadium-core/src/main/java/org/monadium/core/data/Bottom.java
@@ -3,9 +3,9 @@ package org.monadium.core.data;
 public enum Bottom {
 	;
 
-	static <A> A absurd(Bottom b) { return switch (b) { case default -> undefined(); }; }
+	public static <A> A absurd(Bottom b) { return switch (b) { case default -> undefined(); }; }
 
-	static <A, B> B unsafeCoerce(A a) { @SuppressWarnings("unchecked") B b = (B) a; return b; }
-	static class Undefined extends RuntimeException {}
-	static <A> A undefined() throws Undefined { throw new Undefined(); }
+	public static <A, B> B unsafeCoerce(A a) { @SuppressWarnings("unchecked") B b = (B) a; return b; }
+	public static class Undefined extends RuntimeException {}
+	public static <A> A undefined() throws Undefined { throw new Undefined(); }
 }
